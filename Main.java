441abd61d46eb01205af17f7c06ebe5f24dd0d6e
Felipe-Marcello <felipeasmarcello@gmail.com>
2026-09-03 22:02:24 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.multiplicar(10, 5);
+        System.out.println("Resultado: " + resultado);
+    }
+}
