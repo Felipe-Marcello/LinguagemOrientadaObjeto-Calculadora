13c54d8d777603e8ce8fa4cc5c365c37a3168ae3
Felipe-Marcello <felipeasmarcello@gmail.com>
2026-09-03 22:02:56 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,18 @@
+public class Calculadora {
+    public int somar(int numero1, int numero2) {
+        int resultado = numero1 + numero2;
+        return resultado;
+    }
+    public int subitrair(int numero1, int numero2) {
+        int resultado = numero1 - numero2;
+        return resultado;
+    }
+    public int multiplicar(int numero1, int numero2) {
+        int resultado = numero1 * numero2;
+        return resultado;
+    }
+    public double dividir(double numero1, double numero2) {
+        double resultado = numero1 / numero2;
+        return resultado;
+    }
+}
